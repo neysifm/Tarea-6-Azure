10c4a889b12961b873ff18f640a243f5fa7b6749
Neysi Alexandra Fermin Minaya <neysi_fermin@ucne.edu.do>
2019-10-02 11:48:10 -0400

    Estructura de Proyecto Sugerencias

---
+++ Entidades/Sugerencia.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Sugerencia
+    {
+        [Key]
+        public int SugerenciaId { get; set; }
+        public DateTime Fecha { get; set; }
+        public String Descripcion { get; set; }
+
+        public Sugerencia(int sugerenciaId, DateTime fecha, string descripcion)
+        {
+            SugerenciaId = sugerenciaId;
+            Fecha = fecha;
+            Descripcion = descripcion;
+        }
+
+        public Sugerencia()
+        {
+            SugerenciaId = 0;
+            Fecha = DateTime.Now;
+            Descripcion = String.Empty;
+        }
+    }
+}
